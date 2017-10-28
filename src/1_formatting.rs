89dab135153df188cdf91bba32de72296b29bddc
Raj Kiran Gade <rajkiran.gade@gmail.com>
2017-10-29 05:02:17 +0530

    formatting example

--- src/1_formatting.rs
+++ src/1_formatting.rs
@@ -1,3 +1,8 @@
+struct emp {
+name: string,
+no: u32,
+}
+
 fn main() {
 
 // this is simple formatting
@@ -8,5 +13,9 @@ println!("{0}, this is {1} and {1}, this is {0}", "Raj" , "Kiran");
 println!("{arg1} {arg2} {arg3}", arg1="This is arg1", arg2="This is arg2", arg3="This is arg3");
 
 println!("{string:>width$}", string="hello", width=10);
+let employee =  emp{ name: "rk", no: 10};
+
+let (x,y) = employee;
+println!(x);
 
 }
